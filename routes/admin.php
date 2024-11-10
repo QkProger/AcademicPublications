d2926763564a945664885351a94e579912ba6e1b
QkProger <qq13k13k@gmail.com>
2024-11-11 00:42:52 +0500

    graduate start

---
+++ routes/admin.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Controllers\Admin\BannerController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ConverterController;
+use App\Http\Controllers\Admin\LogController;
+use App\Http\Controllers\Admin\MeetingController;
+use App\Http\Controllers\Admin\NewsController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\SolutionController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\Test\TestLanguageController;
+use App\Http\Controllers\Admin\Test\TestSubjectController;
+use App\Http\Controllers\Admin\Test\TestQuestionController;
+use App\Http\Controllers\Admin\Test\FullTestController;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Admin/home');
+})->name('index');
+
+Route::middleware('checkUserRole:admin')->group(function () {
+    Route::resource('roles', RoleController::class)->except(['show'])->names('roles');
+    Route::post('/logs/changeLogStatus', [LogController::class, 'changeLogStatus'])->name('log.changeLogStatus');
+});
+
+Route::middleware('checkUserRole:admin,moderator')->group(function () {
+    Route::resource('user', UserController::class)->except(['show'])->names('users');
+    Route::resource('logs', LogController::class)->only(['index'])->names('logs');
+
+    Route::put('/user/changeLangCode/{id}', [UserController::class, 'changeLangCode'])->name('users.changeLangCode');
+
+});
